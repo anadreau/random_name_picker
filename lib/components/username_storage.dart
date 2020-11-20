da84350d278ff87b0d12f2f2254d5e15c9226dd7
alex nadreau <anadreau42@gmail.com>
2020-11-19 22:48:24 -0700

    Added list function

--- lib/components/username_storage.dart
+++ lib/components/username_storage.dart
@@ -1,11 +1,25 @@
 import 'dart:core';
+import 'dart:io';
 
 List<String> userList = [];
 
-void usernameStorage(){
-  List<String> userList = [];
+void main(){
+  print('Add name');
+  String name = stdin.readLineSync();
+  usernameStorage(name);
+}
+
+
+void usernameStorage(name){
+  //something
+  addUser(name);
+  print(userList);
+  print(userList.length);
 }
 
 void addUser(name){
   userList.add(name);
 }
+
+
+
