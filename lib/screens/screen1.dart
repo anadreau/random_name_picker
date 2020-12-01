a1de11c408b290b5363580c3991b2c821d70b9dc
alex nadreau <anadreau42@gmail.com>
2020-11-30 17:03:09 -0800

    Adding image

--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -15,46 +15,55 @@ class Screen1 extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.orange,
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('UserData'),
-              Text(test.winnerName.toString()),
-              Text(test.userMap.toString()),
-              //Text(test.winnerName),
-              RaisedButton(
-                child: Text('Create file'),
-                onPressed: () {
-                  test.writeFile();
-                  print(dir.path);
-                },
+        body: Row(
+          children: [
+            Center(
+              child: Image(
+                image: Image.file('/images/JB.png'),
               ),
-              RaisedButton(
-                child: Text('Read file'),
-                onPressed: () {
-                  test.readFile(file);
-                },
+            ),
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('UserData'),
+                  Text(test.winnerName.toString()),
+                  Text(test.userMap.toString()),
+                  //Text(test.winnerName),
+                  RaisedButton(
+                    child: Text('Create file'),
+                    onPressed: () {
+                      test.writeFile();
+                      print(dir.path);
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Read file'),
+                    onPressed: () {
+                      test.readFile(file);
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Add name'),
+                    onPressed: () {
+                      test.addName(file, 'Jason');
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Clear List'),
+                    onPressed: () {
+                      test.clearMap();
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Choose Winner'),
+                    onPressed: () => test.chooseWinner(),
+                  ),
+                ],
               ),
-              RaisedButton(
-                child: Text('Add name'),
-                onPressed: () {
-                  test.addName(file, 'Jason');
-                },
-              ),
-              RaisedButton(
-                child: Text('Clear List'),
-                onPressed: () {
-                  test.clearMap();
-                },
-              ),
-              RaisedButton(
-                child: Text('Choose Winner'),
-                onPressed: () => test.chooseWinner(),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
