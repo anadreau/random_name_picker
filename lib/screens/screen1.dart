07ec28aa8a315ec576d10ed0524d2a97e5aaae7d
anadreau <anadreau42@gmail.com>
2020-12-14 17:30:16 -0700

    Changed image to decorationimage to allow overlap of buttons

--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -23,92 +23,101 @@ class Screen1 extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.grey[700],
-        body: Row(
-          children: [
-            Center(child: Image.asset('assets/images/JB.png')),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Goonscription Winner',
-                    style: kGoonBlue,
-                  ),
-                  Text(lotteryList.winnerName.toString(), style: kGoonRed),
-                  Text(lotteryList.userMap.toString(),
-                      softWrap: true, style: kGoonBlue),
-                  //Text(test.winnerName),
-                  RaisedButton(
-                    child: Text('Create file'),
-                    onPressed: () {
-                      lotteryList.writeFile(file);
-                      print(dir.path);
-                    },
-                  ),
-
-                  Container(
-                    height: 40.0,
-                    width: 150.0,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.transparent),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30.0))),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.transparent),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30.0))),
-                        hintText: 'Add Name',
-                        filled: true,
-                        fillColor: Colors.grey[200],
-                        prefixIcon: Icon(Icons.add),
-                      ),
-                      controller: _controller,
-                      focusNode: nameFocusNode,
-                      onSubmitted: (String addName) {
-                        addName = _controller.text;
-                        lotteryList.addName(file, addName);
-                        _controller.clear();
-                        nameFocusNode.requestFocus();
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              alignment: Alignment.centerLeft,
+              image: AssetImage('assets/images/JB.png'),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Center(child: Image.asset('assets/images/JB.png')),
+              Center(
+                child: Column(
+                  //crossAxisAlignment: CrossAxisAlignment.center,
+                  //mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Goonscription Winner',
+                      style: kGoonBlue,
+                    ),
+                    Text(lotteryList.winnerName.toString(), style: kGoonRed),
+                    Text(lotteryList.userMap.toString(),
+                        softWrap: true, style: kGoonBlue),
+                    //Text(test.winnerName),
+                    RaisedButton(
+                      child: Text('Create file'),
+                      onPressed: () {
+                        lotteryList.writeFile(file);
+                        print(dir.path);
                       },
                     ),
-                  ),
-                  // RaisedButton(
-                  //   child: Text('Add name'),
-                  //   onPressed: () {
-                  //     addName = _controller.text;
-                  //     lotteryList.addName(file, addName);
-                  //     _controller.clear();
-                  //   },
-                  // ),
-                  RaisedButton(
-                    child: Text('Read file'),
-                    onPressed: () {
-                      lotteryList.readFile(file);
-                    },
-                  ),
-                  RaisedButton(
-                    child: Text('Clear List'),
-                    onPressed: () {
-                      lotteryList.clearMap();
-                    },
-                  ),
-                  RaisedButton(
-                    child: Text('Choose Winner'),
-                    onPressed: () => lotteryList.chooseWinner(),
-                  ),
-                  // RaisedButton(
-                  //   child: Text('Video Test'),
-                  //   onPressed: () {
-                  //     Navigator.pushNamed(context, Screen2.id);
-                  //   },
-                  // ),
-                ],
+
+                    Container(
+                      height: 40.0,
+                      width: 150.0,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.transparent),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30.0))),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.transparent),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30.0))),
+                          hintText: 'Add Name',
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          prefixIcon: Icon(Icons.add),
+                        ),
+                        controller: _controller,
+                        focusNode: nameFocusNode,
+                        onSubmitted: (String addName) {
+                          addName = _controller.text;
+                          lotteryList.addName(file, addName);
+                          _controller.clear();
+                          nameFocusNode.requestFocus();
+                        },
+                      ),
+                    ),
+                    // RaisedButton(
+                    //   child: Text('Add name'),
+                    //   onPressed: () {
+                    //     addName = _controller.text;
+                    //     lotteryList.addName(file, addName);
+                    //     _controller.clear();
+                    //   },
+                    // ),
+                    RaisedButton(
+                      child: Text('Read file'),
+                      onPressed: () {
+                        lotteryList.readFile(file);
+                      },
+                    ),
+                    RaisedButton(
+                      child: Text('Clear List'),
+                      onPressed: () {
+                        lotteryList.clearMap();
+                      },
+                    ),
+                    RaisedButton(
+                      child: Text('Choose Winner'),
+                      onPressed: () => lotteryList.chooseWinner(),
+                    ),
+                    // RaisedButton(
+                    //   child: Text('Video Test'),
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, Screen2.id);
+                    //   },
+                    // ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
