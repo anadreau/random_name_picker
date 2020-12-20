ad5a8fd200e40245fa21610eff9321a43114bbc2
anadreau <anadreau42@gmail.com>
2020-12-20 12:20:37 -0700

    Added stacks to keep buttons and text synced when resizing window.

--- lib/screens/screen4.dart
+++ lib/screens/screen4.dart
@@ -106,52 +106,81 @@ class Screen4 extends StatelessWidget {
             ),
             LayoutId(
               id: 3, // The id can be anything, i.e. any Object, also an enum value.
-              child: Container(
-                child: Image(
-                  image: AssetImage('assets/images/chat.png'),
-                  height: 200,
-                  width: 400,
-                ),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    height: 600,
+                    width: 400,
+                  ),
+                  Image(
+                    image: AssetImage('assets/images/chat.png'),
+                    height: 200,
+                    width: 400,
+                  ),
+                  Positioned(
+                    top: 70,
+                    left: 110,
+                    child: Text(
+                      lotteryList.winnerName.toString(),
+                      style: kGoonRedStyle,
+                    ),
+                  ),
+                  Positioned(
+                    top: 400,
+                    right: 150,
+                    child: RaisedButton(
+                        child: Text(
+                          'Choose Winner',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        elevation: 10.0,
+                        color: kGoonBlue,
+                        splashColor: kGoonRed,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        onPressed: () {
+                          lotteryList.readFile(file);
+                          lotteryList.chooseWinner();
+                        }),
+                  ),
+                ],
               ), // This is the widget you actually want to show.
             ),
-            LayoutId(
-              id: 2, // You will need to refer to that id when laying out your children.
-              //mainAxisAlignment: MainAxisAlignment.center,
-              child: Column(
-                children: [
-                  // Text(
-                  //   'Goonscription Winner',
-                  //   style: kGoonBlueStyle,
-                  // ),
-                  // Text(lotteryList.winnerName.toString(),
-                  //     style: kGoonRedStyle),
-                  RaisedButton(
-                      child: Text(
-                        'Choose Winner',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      elevation: 10.0,
-                      color: kGoonBlue,
-                      splashColor: kGoonRed,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      onPressed: () {
-                        lotteryList.readFile(file);
-                        lotteryList.chooseWinner();
-                      }),
-                ],
-              ),
-            ),
-            LayoutId(
-              id: 4,
-              child: Text(
-                lotteryList.winnerName.toString(),
-                style: kGoonRedStyle,
-              ),
-            )
+            // LayoutId(
+            //   id: 2, // You will need to refer to that id when laying out your children.
+            //   //mainAxisAlignment: MainAxisAlignment.center,
+            //   child: Column(
+            //     children: [
+            //       RaisedButton(
+            //           child: Text(
+            //             'Choose Winner',
+            //             style: TextStyle(
+            //               fontSize: 18.0,
+            //               fontWeight: FontWeight.bold,
+            //             ),
+            //           ),
+            //           elevation: 10.0,
+            //           color: kGoonBlue,
+            //           splashColor: kGoonRed,
+            //           shape: RoundedRectangleBorder(
+            //               borderRadius: BorderRadius.circular(20)),
+            //           onPressed: () {
+            //             lotteryList.readFile(file);
+            //             lotteryList.chooseWinner();
+            //           }),
+            //     ],
+            //   ),
+            // ),
+            // LayoutId(
+            //   id: 4,
+            //   child: Text(
+            //     lotteryList.winnerName.toString(),
+            //     style: kGoonRedStyle,
+            //   ),
+            // )
           ],
         ),
       ),
@@ -170,17 +199,19 @@ class MyDelegate extends MultiChildLayoutDelegate {
         .zero; // If there is no widget with id `1`, the size will remain at zero.
     // Remember that `1` here can be any **id** - you specify them using LayoutId.
     if (hasChild(1)) {
+      //id 1 is Sock
       leadingSize = layoutChild(
         1, // The id once again.
         BoxConstraints.loose(
             size), // This just says that the child cannot be bigger than the whole layout.
       );
       // No need to position this child if we want to have it at Offset(0, 0).
-      positionChild(1, Offset(0, 0));
+      positionChild(1, Offset(-100, 0));
     }
 
     if (hasChild(2)) {
-      final secondSize = layoutChild(
+      //id 2 is the winner button
+      layoutChild(
         2,
         BoxConstraints(
           // This is exactly the same as above, but this can be anything you specify.
@@ -193,12 +224,13 @@ class MyDelegate extends MultiChildLayoutDelegate {
       positionChild(
         2,
         Offset(
-          leadingSize.width / 1.06, 400, // Centers the second child vertically.
+          leadingSize.width / 2, 400, // Centers the second child vertically.
         ),
       );
     }
     if (hasChild(3)) {
-      final thirdSize = layoutChild(
+      //id 3 is the chat image
+      layoutChild(
         3,
         BoxConstraints(
           // This is exactly the same as above, but this can be anything you specify.
@@ -211,12 +243,13 @@ class MyDelegate extends MultiChildLayoutDelegate {
       positionChild(
         3,
         Offset(
-          leadingSize.width / 1.5, 100, // Centers the second child vertically.
+          leadingSize.width / 2, 50, // Centers the second child vertically.
         ),
       );
     }
     if (hasChild(4)) {
-      final thirdSize = layoutChild(
+      //id 4 is the winner text
+      layoutChild(
         4,
         BoxConstraints(
           // This is exactly the same as above, but this can be anything you specify.
@@ -229,7 +262,7 @@ class MyDelegate extends MultiChildLayoutDelegate {
       positionChild(
         4,
         Offset(
-          leadingSize.width / 1.06, 160, // Centers the second child vertically.
+          leadingSize.width / 1.3, 140, // Centers the second child vertically.
         ),
       );
     }
